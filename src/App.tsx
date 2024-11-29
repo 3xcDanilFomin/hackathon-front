99e2473d0dd3d7af7cf8564b99b50abc9bbd25d1
danil <ap.fomin203@gmail.com>
2024-11-30 01:22:27 +0300

    fix swipe

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import { Provider } from "./components";
 import "./styles/main.scss";
 
 export const App: React.FC = () => {
+  useEffect(() => {
+    if (window.Telegram && window.Telegram.WebApp) {
+      window.Telegram.WebApp.disableVerticalSwipes();
+    }
+  }, []);
   return (
     <>
       <Provider />
